afa609bd1ab5f040c97ffcf9d4ad513398b47537
Pravar Sharma <pcprakash1919@gmail.com>
2025-07-01 10:06:18 +0530

    handeled the ui errors for seperate elements

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,30 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
-import axios from '@/utils/api'
-import { z } from "zod";
+import axios from "@/utils/api";
+import { createIssueSchema } from "../../validationSchemas";
 
-const createIssueSchema = z.object({
-    title: z.string().min(1, "Title is mandatory.").max(255),
-    description: z.string().min(1, "Description is mandatory.")
-})
-
-export async function POST(request: NextRequest){
-    let validation;
-    try{
-        const body = await request.json()
-        validation = createIssueSchema.safeParse(body)
-    }catch (error){
-        console.log(error);
-        return NextResponse.json({"error": "This is a invalid request."}, {status: 400})
-    }
-    if (!validation.success){
-        return NextResponse.json(validation.error.format(), {status: 400})
-    }
-    try {
-        console.log(validation.data)
-        const response = await axios.post("/issues/create_issue", validation.data)
-        console.log(response, "data.....")
-    } catch (error) {
-        return NextResponse.json({"error": "The problem lies with the external server"}, {status: 400})
-    }
-    return NextResponse.json("Success", {status: 201})
+export async function POST(request: NextRequest) {
+  let validation;
+  try {
+    const body = await request.json();
+    validation = createIssueSchema.safeParse(body);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "This is a invalid request." },
+      { status: 400 }
+    );
+  }
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+  try {
+    console.log(validation.data);
+    const response = await axios.post("/issues/create_issue", validation.data);
+    console.log(response, "data.....");
+  } catch (error) {
+    return NextResponse.json(
+      { error: "The problem lies with the external server" },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json("Success", { status: 201 });
 }
--- app/issue/page.tsx
+++ app/issue/page.tsx
@@ -1,44 +1,62 @@
-"use client"
-import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
-import axios from 'axios';
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form';
+"use client";
+import { Button, Callout, Text, TextArea, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-
-interface IssueForm{
-  title: string,
-  description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const IssuePage = () => {
-  const {register, handleSubmit} = useForm<IssueForm>()
-  const [error, setError] = useState('')
-  const onSubmit = async (data: IssueForm)=>{
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const onSubmit = async (data: IssueForm) => {
     try {
-      const response = await axios.post("/api/issues", data)
+      const response = await axios.post("/api/issues", data);
       console.log(response);
     } catch (error) {
-      setError("An unexpected error occured.")
+      setError("An unexpected error occured.");
     }
-  }
-  
+  };
+
   return (
-    <div className='max-w-xl'>
-      {error && <Callout.Root color="red" className='mb-5'>
-            <Callout.Text>
-              {error}
-            </Callout.Text>
-          </Callout.Root>
-      }
-      <form onSubmit={handleSubmit((data)=>onSubmit(data))}>
-        <div className='space-y-3'>
-        <TextField.Root placeholder="Issue Title" {...register("title")}/>
-        <TextArea placeholder="Issue Description" {...register("description")} resize="vertical" />
-        <Button>Submit</Button>
-        </ div>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <div className="space-y-3">
+          <TextField.Root placeholder="Issue Title" {...register("title")} />
+          {errors.title && (
+            <Text color="red" as="p">
+              {errors.title.message}
+            </Text>
+          )}
+          <TextArea
+            placeholder="Issue Description"
+            {...register("description")}
+            resize="vertical"
+          />
+          {errors.description && (
+            <Text color="red" as="p">
+              {errors.description.message}
+            </Text>
+          )}
+          <Button>Submit</Button>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default IssuePage
+export default IssuePage;
